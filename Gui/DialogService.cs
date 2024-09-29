df484099a785579b7eb68a77a3728a8fa8c27c74
kerildash <kerildash@gmail.com>
2024-09-29 21:00:37 +0300

    Add DialogService to show informational dialogs and dialogs for file operations

---
+++ Gui/DialogService.cs
@@ -0,0 +1,72 @@
+﻿﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gui;
+
+public class DialogService
+{
+    public string FilePath { get; set; }
+
+    public bool OpenFile()
+    {
+        try
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text files(*.txt)|*.txt| All files(*.*) |*.*";
+            if (openDialog.ShowDialog() == true)
+            {
+                FilePath = openDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+    public bool SaveFile()
+    {
+        try
+        {
+            if (FilePath == null)
+            {
+                return SaveFileAs();
+            }
+            return true;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+    public bool SaveFileAs()
+    {
+        try
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Text files(*.txt)|*.txt| All files(*.*) |*.*";
+            if (saveDialog.ShowDialog() == true)
+            {
+                FilePath = saveDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+    public void ShowMessage(string message)
+    {
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
