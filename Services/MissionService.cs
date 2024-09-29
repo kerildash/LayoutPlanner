0e10c76f872ac97bbd9a72222697ef237b957e54
kerildash <kerildash@gmail.com>
2024-09-29 20:55:57 +0300

    Add MissionService to deserialize an api response to entities with mission info.

---
+++ Services/MissionService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Info;
+using Newtonsoft.Json.Linq;
+namespace Services;
+
+public class MissionService(HttpService httpService)
+{
+    public async Task<Mission> GetMissionAsync(string uri = "http://promark94.marking.by/client/api/get/task/")
+    {
+
+        Mission mission = null;
+
+        string jsonResponse = await httpService.GetAsync(uri);
+        JObject response = JObject.Parse(jsonResponse);
+
+        JToken result = response["mission"];
+
+        mission = result.ToObject<Mission>();
+        mission.Package = result["lot"]["package"].ToObject<Package>();
+        mission.Product = result["lot"]["product"].ToObject<Product>();
+
+        return mission;
+    }
+}
