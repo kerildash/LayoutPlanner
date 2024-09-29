7dec042147aaf801f3f0e6adb8155dc73d8fc820
kerildash <kerildash@gmail.com>
2024-09-29 20:31:22 +0300

    Add HttpService to send a get request to an api

---
+++ Services/HttpService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace Services;
+
+public class HttpService
+{
+    private readonly HttpClient _client;
+
+    public HttpService()
+    {
+        HttpClientHandler handler = new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.All
+        };
+
+        _client = new HttpClient(handler);
+    }
+
+    public async Task<string> GetAsync(string uri)
+    {
+        HttpResponseMessage response = await _client.GetAsync(uri);
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
