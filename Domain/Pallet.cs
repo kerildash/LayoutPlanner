d9353106d589eff1b8f46ecf60f006d59eb7d0d9
kerildash <kerildash@gmail.com>
2024-09-30 17:27:19 +0300

    Add a method to add an item to the pallet. Add a method to check if the pallet is full.

--- Domain/Pallet.cs
+++ Domain/Pallet.cs
@@ -12,6 +12,11 @@ public class Pallet
         Id = Guid.NewGuid();
         Code = CreateCode(gtin, capacity);
         Capacity = capacity;
+        Boxes = [];
+    }
+    public bool IsFull()
+    {
+        return Boxes.Any() && Boxes.Count == Capacity && Boxes.Last().IsFull();
     }
     public void AddBox(Box box)
     {
@@ -23,6 +28,25 @@ public class Pallet
         {
             throw new InvalidOperationException("Cannot load the box to the pallet: this pallet is full");
         }
+    }
+    public void AddItemToPallet(Item item, string gtin, int boxFormat)
+    {
+        if (Boxes.Any() && !Boxes.Last().IsFull())
+        {
+            Boxes.Last().AddItem(item);
+        }
+        else
+        {
+            Box newBox = new Box(gtin, boxFormat);
+            newBox.Pallet = this;
+            newBox.AddItem(item);
+            Boxes.Add(newBox);
+        }
+    }
+    public void AddItemToLastBox(Item item)
+    {
+        Box box = Boxes.Last();
+        box.AddItem(item);
     }
     private string CreateCode(string gtin, int capacity)
     {
