db43e8dd67f2aa2c3d48f04e593c385ba64cfe0a
kerildash <kerildash@gmail.com>
2024-10-02 22:54:10 +0300

    Change Layout: add iterator methods

--- Domain/Layout.cs
+++ Domain/Layout.cs
@@ -2,19 +2,60 @@
 
 public class Layout
 {
+    public string ProductName { get; set; }
     public string Gtin { get; }
     public int BoxFormat { get; }
 
     public int PalletFormat { get; }
 
-    public List<Pallet> Pallets { get; private set; }
-    public Layout(string gtin, int boxFormat, int palletFormat)
+    public List<Pallet> Pallets { get; set; }
+    public Layout()
     {
+        Pallets = [];
+    }
+    public Layout(string productName, string gtin, int boxFormat, int palletFormat)
+    {
+        ProductName = productName;
+        Pallets = [];
         Gtin = gtin;
         BoxFormat = boxFormat;
         PalletFormat = palletFormat;
-        Pallets = [];
     }
+
+    #region iterators
+
+    public IEnumerable<Pallet> GetPallets()
+    {
+        //if (Pallets.Any()) yield return new Pallet { };
+        foreach (var pallet in Pallets)
+        {
+            yield return pallet;
+        }
+    }
+
+    public IEnumerable<Box> GetBoxes()
+    {
+        foreach (var pallet in Pallets)
+        {
+            foreach (var box in pallet.Boxes)
+            {
+                yield return box;
+            }
+        }
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+        foreach(var box in GetBoxes())
+        {
+            foreach(var item in box.Items)
+            {
+                yield return item;
+            }
+        }
+    }
+    #endregion
+
 
     public void AddItem(Item item)
     {
