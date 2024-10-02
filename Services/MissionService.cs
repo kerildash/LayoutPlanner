821764bfed2a2c2cc2246909a197da237947c031
kerildash <kerildash@gmail.com>
2024-10-02 22:56:18 +0300

    Change MissionService: add method to insert entities to a database

--- Services/MissionService.cs
+++ Services/MissionService.cs
@@ -1,9 +1,14 @@
 ﻿using Domain.Info;
 using Domain;
 using Newtonsoft.Json.Linq;
+using Database.Repositories;
 namespace Services;
 
-public class MissionService(HttpService httpService)
+public class MissionService(
+    HttpService httpService,
+    IRepository<Pallet> palletRepository,
+    IRepository<Box> boxRepository,
+    IItemRepository itemRepository)
 {
     public async Task<Mission> GetMissionAsync(string uri = "http://promark94.marking.by/client/api/get/task/")
     {
@@ -28,16 +33,40 @@ public class MissionService(HttpService httpService)
         using Stream stream = File.OpenRead(path);
         using StreamReader reader = new(stream);
 
-        Layout layout = new(mission.Product.Gtin, mission.Package.BoxFormat, mission.Package.PalletFormat);
+        Layout layout = new(
+                mission.Product.Name,
+                mission.Product.Gtin,
+                mission.Package.BoxFormat,
+                mission.Package.PalletFormat);
 
         while ((line = await reader.ReadLineAsync()) != null)
         {
-            if (line.Contains(mission.Product.Gtin))
+            if (line.Contains(mission.Product.Gtin) && !itemRepository.Exists(line))
             {
-                Item item = new Item { Code = line, Id = Guid.NewGuid() };
+                Item item = new Item { Code = line };
                 layout.AddItem(item);
             }
         }
+        InsertLayoutToDatabase(layout);
         return layout;
+    }
+
+    public void InsertLayoutToDatabase(Layout layout)
+    {
+        foreach (Pallet pallet in layout.GetPallets())
+        {
+            palletRepository.AddAsync(pallet);
+
+        }
+        foreach (Box box in layout.GetBoxes())
+        {
+            boxRepository.AddAsync(box);
+        }
+        foreach (Item item in layout.GetItems())
+        {
+            itemRepository.AddAsync(item);
+        }
+        itemRepository.SaveChanges();
+
     }
 }
