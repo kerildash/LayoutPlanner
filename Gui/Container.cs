153a7532236d98f92e8b7a763d07899ba5b98341
kerildash <kerildash@gmail.com>
2024-10-02 23:07:24 +0300

    Move configuring of a DI services provider to another class (Container) to clear the code-behind. Add Container as a static resource to App.xaml. Update bindings in MainVindow.xaml

---
+++ Gui/Container.cs
@@ -0,0 +1,45 @@
+﻿using Database.Repositories;
+using Database;
+using Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace Gui;
+
+public class Container
+{
+    public IServiceProvider ServiceProvider { get; private set; }
+
+
+    public Container()
+    {
+        ServiceCollection services = new();
+        services.AddTransient<HttpService>();
+        services.AddTransient<MissionService>();
+
+        services.AddScoped<DialogService>();
+        services.AddSingleton<MainWindowViewModel>();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(
+                ConfigurationManager.AppSettings["DefaultConnection"]);
+        });
+
+        services.AddScoped<IRepository<Pallet>, PalletRepository>();
+        services.AddScoped<IRepository<Box>, BoxRepository>();
+        services.AddScoped<IItemRepository, ItemRepository>();
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        ServiceProvider = services.BuildServiceProvider();
+
+        services.AddScoped<ServiceProvider>();
+
+        MainWindowViewModel = ServiceProvider.GetRequiredService<MainWindowViewModel>();
+    }
+
+    public MainWindowViewModel MainWindowViewModel { get; private set; }
+}
--- Gui/MainWindow.xaml.cs
+++ Gui/MainWindow.xaml.cs
@@ -1,29 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Services;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Gui;
 
 
 public partial class MainWindow : Window
 {
-    public IServiceProvider ServiceProvider { get; private set; }
 
-    public MainWindow()
-    {
-        ServiceCollection services = new();
-        services.AddTransient<HttpService>();
-        services.AddTransient<MissionService>();
-
-        services.AddScoped<DialogService>();
-        services.AddSingleton<MainWindowViewModel>();
-
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        ServiceProvider = services.BuildServiceProvider();
-
-        services.AddScoped<ServiceProvider>();
-        MainWindowViewModel viewModel = ServiceProvider.GetRequiredService<MainWindowViewModel>();
-        DataContext = viewModel;
-    }
 }
